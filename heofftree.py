2b6980cd29eb677ae7c5750b79a88b561e708f46
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-03-28 19:52:51 +0200

    Added HT and HST

--- heofftree.py
+++ heofftree.py
@@ -1,16 +1,19 @@
 import data
 
-from pprint import pprint
-from river import compose
 from river import tree
+from river import compose
+from river import metrics
+from river import evaluate
 
 dataset = data.labeled_901()
 
 model = compose.Pipeline(
     compose.Select("year", "month", "day", "hour", "minute", "second", "week", "weekOrder", "ammonium_901", "conductivity_901", "dissolved_oxygen_901", "ph_901", "turbidity_901", "water_temperature_901"),
-    tree.HoeffdingTreeClassifier(grace_period=50)
+    tree.HoeffdingTreeClassifier(grace_period=200)
 )
 
-for x, y in dataset:
-    model.learn_one(x, y)
-    # print(model.debug_one(x))
+metric = metrics.Accuracy()
+
+evaluate.progressive_val_score(dataset, model, metric)
+
+print(metric)
--- logreg.py
+++ logreg.py
@@ -114,25 +114,25 @@ def imputation_trees(station):
 
 if __name__ == '__main__':
 
-    imputation_knn(station=901)
+    # imputation_knn(station=901)
 
-    # dataset = data.labeled_901()
+    dataset = data.labeled_901()
 
-    # from river import compose
-    # from river import metrics
-    # from river import linear_model
+    from river import compose
+    from river import metrics
+    from river import linear_model
 
-    # model = compose.Pipeline(
-    #     compose.Select("year", "month", "day", "hour", "minute", "second", "week", "weekOrder", "ammonium_901", "conductivity_901", "dissolved_oxygen_901", "ph_901", "turbidity_901", "water_temperature_901"),
-    #     linear_model.LogisticRegression()
-    # )
+    model = compose.Pipeline(
+        compose.Select("year", "month", "day", "hour", "minute", "second", "week", "weekOrder", "ammonium_901", "conductivity_901", "dissolved_oxygen_901", "ph_901", "turbidity_901", "water_temperature_901"),
+        linear_model.LogisticRegression()
+    )
 
-    # metric = metrics.ROCAUC()
+    metric = metrics.ROCAUC()
 
-    # for x, y in dataset:
-    #     y_pred = model.predict_proba_one(x)
-    #     model.learn_one(x, y)
-    #     metric.update(y, y_pred)
-    #     # print(model.debug_one(x))
+    for x, y in dataset:
+        y_pred = model.predict_proba_one(x)
+        model.learn_one(x, y)
+        metric.update(y, y_pred)
+        # print(model.debug_one(x))
 
-    # print(metric)
+    print(metric)
