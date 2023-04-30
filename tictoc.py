9127aeed0232437cd80aab80bdd74b2deae0769b
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-04-30 21:37:02 +0200

    Finished combinatorics

--- tictoc.py
+++ tictoc.py
@@ -3,7 +3,8 @@ import time
 def tictoc(func):
     def wrapper(*args, **kargs):
         t1 = time.time()
-        func(*args, **kargs)
+        result = func(*args, **kargs)
         t2 = time.time() - t1
         print(f'{func.__name__} ran in {round(t2, ndigits=2)} seconds')
+        return result
     return wrapper
